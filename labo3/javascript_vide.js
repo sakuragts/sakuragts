e58c073e4c00db2bbea4050c507a6f1e8582ebcd
sakuragts <sakura_gts@hotmail.com>
2021-06-09 17:53:21 -0400

    adding labo3

---
+++ labo3/javascript_vide.js
@@ -0,0 +1,24 @@
+var nombre1;
+var nombre2;
+var operation;
+var resultat;
+
+function changerCouleur(couleur) {
+	/* Code à compléter */
+}
+
+function ajouterChiffre(chiffre) {
+	nombre1 = chiffre + nombre1;
+}
+
+function ajouterOperation(operation) {
+	/* Code à compléter */
+}
+
+function effacer() {
+	/* Code à compléter */
+}
+
+function resoudre() {
+   /* Code à compléter */
+}
